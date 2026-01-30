0e3f3b868bd7941be01b899b1d1a2630e66d31c6
Swifter <swifter1243@gmail.com>
2026-01-30 04:38:12 -0500

    content page vertical slice

--- src/nodes/project/create.ts
+++ src/nodes/project/create.ts
@@ -1,9 +1,10 @@
 import { makeProject, type Project } from "./project";
 import placeHolderHTML from '../pages/placeholder.html?raw'
+import extraSensory2HTML from './pages/extra_sensory_2.html?raw'
 
 export function createProjects(): Project[] {
     return [
-        makeProject('Extra Sensory II', 'extra-sensory-ii', placeHolderHTML, ['Games', 'Art'], ['C#', 'Shaders', 'Unity', 'TypeScript', 'Teamwork']),
+        makeProject('Extra Sensory II', 'extra-sensory-ii', extraSensory2HTML, ['Games', 'Art'], ['C#', 'Shaders', 'Unity', 'TypeScript', 'Teamwork']),
         makeProject('Derelict', 'derelict', placeHolderHTML, ['Games'], ['C#', 'Unity', 'Teamwork', 'Shaders']),
         makeProject('SYNERGY', 'synergy', placeHolderHTML, ['Games', 'Tools'], ['C#', 'Unity', 'Shaders']),
         makeProject('743⁺Aether*✧ . * ¹¹¹} ⁺ , .', 'aether', placeHolderHTML, ['Art'], ['C#', 'Shaders', 'Unity']),
