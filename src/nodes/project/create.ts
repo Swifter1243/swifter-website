1904c6572fca80062916573dcbbed741e6800188
Swifter <swifter1243@gmail.com>
2026-01-31 20:09:31 -0500

    add aether project

--- src/nodes/project/create.ts
+++ src/nodes/project/create.ts
@@ -2,13 +2,14 @@ import { makeProject, type Project } from "./project";
 import placeHolderHTML from '../pages/placeholder.html?raw'
 import extraSensory2HTML from './pages/extra_sensory_2.html?raw'
 import derelictHTML from './pages/derelict.html?raw'
+import aetherHTML from './pages/aether.html?raw'
 
 export function createProjects(): Project[] {
     return [
         makeProject(5, 'Extra Sensory II', 'extra-sensory-ii', extraSensory2HTML, 'Experiences', ['C#', 'Shaders', 'Unity', 'TypeScript', 'Teamwork']),
         makeProject(4, 'Derelict', 'derelict', derelictHTML, 'Games', ['C#', 'Unity', 'Teamwork', 'Shaders']),
         makeProject(4, 'SYNERGY', 'synergy', placeHolderHTML, 'Games', ['C#', 'Unity', 'Shaders']),
-        makeProject(3, '743⁺Aether*✧ . * ¹¹¹} ⁺ , .', 'aether', placeHolderHTML, 'Experiences', ['C#', 'Shaders', 'Unity']),
+        makeProject(3, '743⁺Aether*✧ . * ¹¹¹} ⁺ , .', 'aether', aetherHTML, 'Experiences', ['C#', 'Shaders', 'Unity']),
         makeProject(2, 'Grasping At Straws', 'grasping-at-straws', placeHolderHTML, 'Games', ['C#', 'Shaders', 'Unity']),
         makeProject(2, 'ZERO Inc.', 'zero-inc', placeHolderHTML, 'Games', ['C#', 'Shaders', 'Teamwork']),
         makeProject(2, 'BLOON MAN', 'bloon-man', placeHolderHTML, 'Games', ['C#', 'Teamwork', 'Unity']),
@@ -29,5 +30,6 @@ export function createProjects(): Project[] {
         makeProject(2, 'Greenfoot Tetris', 'magic-nether-portal', placeHolderHTML, 'Games', ['Java']),
         makeProject(1.5, 'Music Sketch Catalogue', 'music-sketch-catalogue', placeHolderHTML, 'Experiments', ['TypeScript']),
         makeProject(5, 'BoomBox Remastered', 'boombox-remastered', placeHolderHTML, 'Games', ['Unity', 'C#', 'Shaders', 'Teamwork']),
+        makeProject(2, 'Unity Animation Window', 'unity-animation-window', placeHolderHTML, 'Tools', ['Unity', 'C#']),
     ]
 }
