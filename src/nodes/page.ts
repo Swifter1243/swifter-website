14d246f3e5b4fe4793925b91305d0473a7fe9078
Swifter <swifter1243@gmail.com>
2026-03-13 18:19:43 -0400

    fix page imports for build

--- src/nodes/page.ts
+++ src/nodes/page.ts
@@ -5,38 +5,39 @@ export type Page = {
     loaded?: Component
 }
 
-function makePage(url: string): Page {
+function makePage(loader: () => Promise<any>): Page {
     return {
-        loadMethod: () => import(url).then(m => m.default)
-    }
+        loadMethod: () => loader().then(m => m.default)
+    };
 }
 
 export const PAGES = {
-    placeHolder: makePage("/src/lib/pages/PlaceHolder.svelte"),
-    aboutMe: makePage("/src/lib/pages/AboutMe.svelte"),
-    myLinks: makePage("/src/lib/pages/MyLinks.svelte"),
-    aether: makePage("/src/lib/pages/projects/Aether.svelte"),
-    beatSaberEnvironmentGrabber: makePage("/src/lib/pages/projects/BeatSaberEnvironmentGrabber.svelte"),
-    beatCraft: makePage("/src/lib/pages/projects/BeatCraft.svelte"),
-    beatSaberLevels: makePage("/src/lib/pages/projects/BeatSaberLevels.svelte"),
-    bloodRushing: makePage("/src/lib/pages/projects/BloodRushing.svelte"),
-    bloonMan: makePage("/src/lib/pages/projects/BloonMan.svelte"),
-    boomboxRemastered: makePage("/src/lib/pages/projects/BoomboxRemastered.svelte"),
-    derelict: makePage("/src/lib/pages/projects/Derelict.svelte"),
-    enlighten: makePage("/src/lib/pages/projects/Enlighten.svelte"),
-    excelSoftwareRenderer: makePage("/src/lib/pages/projects/ExcelSoftwareRenderer.svelte"),
-    extraSensory2: makePage("/src/lib/pages/projects/ExtraSensory2.svelte"),
-    graspingAtStraws: makePage("/src/lib/pages/projects/GraspingAtStraws.svelte"),
-    greenfootTetris: makePage("/src/lib/pages/projects/GreenfootTetris.svelte"),
-    magicNetherPortal: makePage("/src/lib/pages/projects/MagicNetherPortal.svelte"),
-    myMusic: makePage("/src/lib/pages/projects/MyMusic.svelte"),
-    plasmaShift: makePage("/src/lib/pages/projects/PlasmaShift.svelte"),
-    remapper: makePage("/src/lib/pages/projects/ReMapper.svelte"),
-    ricochet: makePage("/src/lib/pages/projects/Ricochet.svelte"),
-    sdlMinecraft: makePage("/src/lib/pages/projects/SDLMinecraft.svelte"),
-    shadertoyShaders: makePage("/src/lib/pages/projects/ShadertoyShaders.svelte"),
-    synergy: makePage("/src/lib/pages/projects/Synergy.svelte"),
-    unityAnimationWindow: makePage("/src/lib/pages/projects/UnityAnimationWindow.svelte"),
-    vivifyTemplate: makePage("/src/lib/pages/projects/VivifyTemplate.svelte"),
-    zeroInc: makePage("/src/lib/pages/projects/ZeroInc.svelte"),
+    placeHolder: makePage(() => import("$lib/pages/PlaceHolder.svelte")),
+    aboutMe: makePage(() => import("$lib/pages/AboutMe.svelte")),
+    myLinks: makePage(() => import("$lib/pages/MyLinks.svelte")),
+
+    aether: makePage(() => import("$lib/pages/projects/Aether.svelte")),
+    beatSaberEnvironmentGrabber: makePage(() => import("$lib/pages/projects/BeatSaberEnvironmentGrabber.svelte")),
+    beatCraft: makePage(() => import("$lib/pages/projects/BeatCraft.svelte")),
+    beatSaberLevels: makePage(() => import("$lib/pages/projects/BeatSaberLevels.svelte")),
+    bloodRushing: makePage(() => import("$lib/pages/projects/BloodRushing.svelte")),
+    bloonMan: makePage(() => import("$lib/pages/projects/BloonMan.svelte")),
+    boomboxRemastered: makePage(() => import("$lib/pages/projects/BoomboxRemastered.svelte")),
+    derelict: makePage(() => import("$lib/pages/projects/Derelict.svelte")),
+    enlighten: makePage(() => import("$lib/pages/projects/Enlighten.svelte")),
+    excelSoftwareRenderer: makePage(() => import("$lib/pages/projects/ExcelSoftwareRenderer.svelte")),
+    extraSensory2: makePage(() => import("$lib/pages/projects/ExtraSensory2.svelte")),
+    graspingAtStraws: makePage(() => import("$lib/pages/projects/GraspingAtStraws.svelte")),
+    greenfootTetris: makePage(() => import("$lib/pages/projects/GreenfootTetris.svelte")),
+    magicNetherPortal: makePage(() => import("$lib/pages/projects/MagicNetherPortal.svelte")),
+    myMusic: makePage(() => import("$lib/pages/projects/MyMusic.svelte")),
+    plasmaShift: makePage(() => import("$lib/pages/projects/PlasmaShift.svelte")),
+    remapper: makePage(() => import("$lib/pages/projects/ReMapper.svelte")),
+    ricochet: makePage(() => import("$lib/pages/projects/Ricochet.svelte")),
+    sdlMinecraft: makePage(() => import("$lib/pages/projects/SDLMinecraft.svelte")),
+    shadertoyShaders: makePage(() => import("$lib/pages/projects/ShadertoyShaders.svelte")),
+    synergy: makePage(() => import("$lib/pages/projects/Synergy.svelte")),
+    unityAnimationWindow: makePage(() => import("$lib/pages/projects/UnityAnimationWindow.svelte")),
+    vivifyTemplate: makePage(() => import("$lib/pages/projects/VivifyTemplate.svelte")),
+    zeroInc: makePage(() => import("$lib/pages/projects/ZeroInc.svelte")),
 } satisfies Record<string, Page>
