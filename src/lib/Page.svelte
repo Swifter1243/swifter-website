e023b02b2dac16a0bd1d61ea2fec19579d9994fe
Swifter <swifter1243@gmail.com>
2026-03-12 22:12:21 -0400

    reroute 404 pages to main page

---
+++ src/lib/Page.svelte
@@ -0,0 +1,27 @@
+<title id="title">PORTFOLIO</title>
+
+<script lang="ts">
+    import { onMount } from "svelte"
+    import { initSound } from "../view/sound/main"
+    import { initThree } from "../view/three/main"
+    import { initSystems } from "../initialize"
+    import { fadeIn } from "../view/fade"
+    import PagePanel from "$lib/components/PagePanel.svelte";
+
+    onMount(async () => {
+        await initSound()
+        await initThree()
+        initSystems()
+        fadeIn()
+    })
+</script>
+
+<div id="three"></div>
+
+<div id="tutorial">
+    <p id="tutorial-text"></p>
+</div>
+
+<PagePanel></PagePanel>
+
+<div id="fade"></div>
